d7a1fb2c6a47aaa2245329de353e09ceec99c20b
Crowdbotics <team@crowdbotics.com>
2023-10-07 19:20:10 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "round-king-43819",
+  url: "https://round-king-43819.botics.co",
+  api: "https://round-king-43819.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
